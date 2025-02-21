105203725077b537d45f20fadf52907da3906339
Devendra6666 <2019yoga@gmail.com>
2025-02-22 00:23:16 +0530

    the code edited success

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! this is a testing application for EBS'));
+app.get('/', (req, res) => res.send('Hello World! Allset up for the CI/CD done in aws'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
